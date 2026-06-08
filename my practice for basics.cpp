b84d8d63cff692e57d50e08978e28b1448684f66
Rahul <217455023+codingwithrahul965@users.noreply.github.com>
2026-06-08 16:53:46 +0530

    new problems done

---
+++ my practice for basics.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;        
+int main() {
+    int ac =10;
+    float y = 10.90;
+    cout<<ac<<endl;
+    cout<<y<<endl;
+
+
+    string name = "satyarth";
+    cout<<name<<endl;
+
+
+    string str;
+    getline(cin,str);
+    cout<<str<<endl;
+
+
+    int day;
+    cin>>day;
+    switch(day){
+        case 1: cout<<"Monday";
+                break;
+        case 2: cout<<"Tuesday";
+                break;
+        case 3: cout<<"Wednesday";
+                break;
+        case 4: cout<<"Thursday";
+                break;
+        case 5: cout<<"Friday";
+                break;
+        case 6: cout<<"Saturday";
+                break;
+        case 7: cout<<"Sunday";
+                break;
+        default: cout<<"Invalid day";
+    }
+
+    
+
+
+    
+
+    return 0;
+}
---
+++ striver sheets problem.cpp
@@ -0,0 +1,44 @@
+// CHAPTER 1: BASICS OF C++
+
+#include <iostream>
+using namespace std;
+
+// class Solution {
+// public:
+//     void studentGrade(int marks) {
+//         if (marks >= 90)
+//             cout << "Grade A";
+//         else if (marks >= 70)
+//             cout << "Grade B";
+//         else if (marks >= 50)
+//             cout << "Grade C";
+//         else if (marks >= 35)
+//             cout << "Grade D";
+//         else
+//             cout << "Fail";
+//     }
+// };
+
+// int main() {
+//     Solution obj;
+//     obj.studentGrade(85);
+//     return 0;
+// }
+
+
+
+    // int main(){
+    //     int low;
+    //     int high;
+    //     cin>>low;
+    //     cin>>high;
+    //     int sum=0;
+    //     for(int i=low;i<=high;i++){
+    //         sum=sum+i;}
+    //     cout<<sum<<"";    
+        
+    
+    //     return 0;
+    // };
+
+
